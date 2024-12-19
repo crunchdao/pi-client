75626cecbd57fba35d9f791f622b7c9d52c89737
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-19 15:52:55 +0100

    feat(api): add question listing endpoint

--- pi/api/client.py
+++ pi/api/client.py
@@ -4,11 +4,13 @@ import typing
 import urllib.parse
 
 import inflection
+import pandas
 import requests
 
 from .. import constants
 from .errors import *
 from .models import *
+from .pagination import *
 
 
 class Client:
@@ -17,6 +19,7 @@ class Client:
         self,
         api_key: typing.Optional[str] = None,
         base_url: typing.Optional[str] = None,
+        page_size: int = 100,
     ):
         if api_key is None:
             api_key = os.environ.get(constants.API_KEY_ENVVAR)
@@ -25,6 +28,7 @@ class Client:
             base_url = os.environ.get(constants.BASE_URL_ENVVAR) or constants.BASE_URL_DEFAULT
 
         self._base_url = base_url
+        self._page_size = page_size
 
         self._session = requests.Session()
         self._session.params.update({
@@ -63,6 +67,28 @@ class Client:
         }
 
         return error_class(message, **args)
+
+    def _paginate(
+        self,
+        requester: typing.Callable[[PageRequest], requests.Response],
+        start_page: int = 0,
+        page_size: typing.Optional[int] = None,
+    ):
+        if not page_size:
+            page_size = self._page_size
+
+        page_request = PageRequest(start_page, page_size)
+        while True:
+            page_response = requester(page_request)
+
+            content = page_response["content"]
+            for item in content:
+                yield item
+
+            if len(content) != page_response["pageSize"]:
+                break
+
+            page_request = page_request.next()
 
     def get_current_user(self) -> CurrentUser:
         content = self._request("GET", "v1/users/@me")
@@ -121,6 +147,87 @@ class Client:
                 wait -= 1
 
         return question
+
+    @typing.overload
+    def list_questions(
+        self,
+        *,
+        as_dataframe: typing.Literal[False] = False,
+    ) -> typing.Iterator[Question]:
+        ...
+
+    @typing.overload
+    def list_questions(
+        self,
+        *,
+        as_dataframe: typing.Literal[True] = True,
+    ) -> pandas.DataFrame:
+        ...
+
+    def list_questions(
+        self,
+        *,
+        as_dataframe: bool = False,
+        only_successful: typing.Optional[bool] = None,
+        user_id: typing.Optional[int] = None,
+        tags: typing.Optional[typing.List[str]] = None,
+        datasource_name: typing.Optional[str] = None,
+        created_after: typing.Optional[datetime.datetime] = None,
+        created_before: typing.Optional[datetime.datetime] = None,
+        vote_direction: typing.Optional[typing.Literal["ANY", "UP", "DOWN"]] = None,
+        sort_by: typing.Optional[typing.Literal["HIGHER_SCORE", "HIGHER_CORRELATION", "HIGHER_REWARDED", "RECENT"]] = None,
+        start_page: typing.Optional[int] = 0,
+    ) -> typing.Union[typing.Iterator[Question] | pandas.DataFrame]:
+        params = {}
+
+        if only_successful is not None:
+            params["onlySuccessful"] = only_successful
+
+        if user_id is not None:
+            params["userId"] = user_id
+
+        if tags is not None:
+            params["tags"] = tags
+
+        if datasource_name is not None:
+            params["datasource"] = datasource_name
+
+        if created_after is not None:
+            params["createdAfter"] = created_after.isoformat()
+
+        if created_before is not None:
+            params["createdBefore"] = created_before.isoformat()
+
+        if vote_direction is not None:
+            params["voteDirection"] = vote_direction
+
+        if sort_by is not None:
+            params["sortBy"] = sort_by
+
+        paginated = self._paginate(
+            lambda page_request: self._request(
+                "GET",
+                "/v1/questions",
+                params={
+                    **params,
+                    "page": page_request.number,
+                    "size": page_request.size,
+                },
+            ),
+            start_page=start_page,
+        )
+
+        if as_dataframe:
+            return pandas.json_normalize([
+                dataclasses.asdict(Question.from_dict(item))
+                for item in paginated
+            ])
+        else:
+            def mapper():
+                for item in paginated:
+                    yield Question.from_dict(item)
+
+            return mapper()
 
     def get_question(
         self,
