ea81ee2639325686bd0b2b969b732963f16b1f7e
Caceresenzo <caceresenzo1502@gmail.com>
2025-05-07 14:29:30 +0200

    release: bump to 0.2.1

--- pi/__version__.py
+++ pi/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'pi'
 __description__ = 'pi - Client for the CrunchDAO Pi Project!'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Enzo CACERES'
 __author_email__ = 'enzo.caceres@crunchdao.com'
 __url__ = 'https://github.com/crunchdao/pi-client'
